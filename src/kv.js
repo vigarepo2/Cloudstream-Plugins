e612b9cb3b6eb7d998ab1ebca019e39bd0f477fb
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2026-03-22 17:26:54 +0530

    Update kv.js

--- src/kv.js
+++ src/kv.js
@@ -7,6 +7,7 @@ export async function setCache(kv, key, value, ttlSeconds = 604800) {
 }
 
 export async function clearUserCaches(kv, username) {
+  await kv.delete(`repo_${username}_all`);
   await kv.delete(`repo_${username}_sfw`);
   await kv.delete(`repo_${username}_nsfw`);
   await kv.delete(`user_plugins_${username}`);
